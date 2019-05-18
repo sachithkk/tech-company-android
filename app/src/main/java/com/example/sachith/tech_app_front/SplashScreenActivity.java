3bfe33808448cd2ad84dff663ac86d79f61da0c0
sajith priyankara <sajith1995s@gmail.com>
2019-05-18 17:05:34 +0530

    add splash screen

---
+++ app/src/main/java/com/example/sachith/tech_app_front/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.example.sachith.tech_app_front;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen splashScreen = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(HomeCompany.class)
+                .withSplashTimeOut(5000)
+                .withBackgroundColor(Color.parseColor("#95a5a6"))
+                .withLogo(R.mipmap.logo)
+                .withAfterLogoText("TechCom");
+
+
+        View splashView = splashScreen.create();
+        setContentView(splashView);
+    }
+}
